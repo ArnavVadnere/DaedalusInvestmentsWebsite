016c3af41193a9b81f699613512764270b8c0303
Arnav Vadnere <arnavvadnere@gmail.com>
2021-06-15 00:11:47 -0400

    commit3

--- DaedalusInvestments/DaedalusInvestments/settings.py
+++ DaedalusInvestments/DaedalusInvestments/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w5**lqsfs)vr&6(c35ix16($z=f-nyf63!wzlega9myf=rc-mm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['your_app_name.herokuapp.com', 'localhost', '127.0.0.1'] in settings.py
+
 
 
 # Application definition
@@ -122,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
